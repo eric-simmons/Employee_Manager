4bdd541c0db24916d229a5527ae97119100a929d
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-11-21 13:01:55 -0600

    init question

--- index.js
+++ index.js
@@ -2,3 +2,26 @@ require('dotenv').config()
 const inquirer = require('inquirer')
 const cTable = require('console.table')
 const connection = require('./config/connection.js')
+const { initQuestion, departmentQuestions, employeeQuestions } = require('./lib/questions.js')
+
+
+const init = async () => {
+    let answer = await inquirer.prompt(initQuestion)
+
+    if (answer.action === "View all Employees") {
+        showEmployees()
+    }
+
+}
+init()
+
+
+const showEmployees = async () => {
+    try {
+        const [results] = await connection.promise().query(`SELECT * FROM employees`)
+        console.table(results)
+    }
+    catch (error) {
+        throw new Error(error)
+    }
+}
