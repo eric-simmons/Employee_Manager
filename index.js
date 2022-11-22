7215cf69af4bb3a7d45c0bc06472eb74f2f3fbb5
Eric Simmons <erictomlinsonsimmons@gmail.com>
2022-11-21 18:35:13 -0600

    map

--- index.js
+++ index.js
@@ -5,23 +5,45 @@ const connection = require('./config/connection.js')
 const { initQuestion, departmentQuestions, employeeQuestions } = require('./lib/questions.js')
 
 
-const init = async () => {
-    let answer = await inquirer.prompt(initQuestion)
-
-    if (answer.action === "View all Employees") {
-        showEmployees()
-    }
-
-}
-init()
 
 
-const showEmployees = async () => {
+const showRoles = async () => {
     try {
-        const [results] = await connection.promise().query(`SELECT * FROM employees`)
+        const [results] = await connection.promise().query(`SELECT * FROM roles`)
         console.table(results)
+        init()
     }
     catch (error) {
         throw new Error(error)
     }
 }
+
+const showEmployees = async () => {
+    try {
+        const [results] = await connection.promise().query(`SELECT * FROM employees`)
+        console.table(results)
+        init()
+    }
+    catch (error) {
+        throw new Error(error)
+    }
+}
+
+const mapActions = {
+    'View all Employees': showEmployees,
+    'View all Roles' : showRoles,
+}
+
+const init = async () => {
+    let answer = await inquirer.prompt([{
+        type: "list",
+        name: "action",
+        message: "What would you like to do?",
+        choices: Object.keys(mapActions)
+    }])
+    console.log(answer)
+    console.log(mapActions[answer.action])
+    mapActions[answer.action]()
+}
+
+init()
